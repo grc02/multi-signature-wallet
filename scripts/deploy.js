31343ea9d4204ec7c9c9a2500cf335435d78083b
grc02 <georgi.chonkov27@gmail.com>
2022-06-03 11:49:31 +0300

    completed the scripts/deploy.js file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 const main = async() => {
    const contractFactory = await ethers.getContractFactory('MultiSigWallet');
-   const contract = await contractFactory.deploy();
+   // sample addresses from the Remix IDE (first 3 addresses)
+   const contract = await contractFactory.deploy(["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"], 2);
    await contract.deployed();
  
    console.log("Contract deployed to: ", contract.address);
